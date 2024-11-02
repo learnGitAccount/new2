370534ede46f890f8ac5e53e5ec1b3bdab302f4f
DESKTOP-P2V9UA1\SomeOne <molaei1999a@gmail.com>
2024-11-02 12:06:47 +0330

    add logic to the header section

--- assets/main.js
+++ assets/main.js
@@ -2,3 +2,9 @@
 // this is the navbar logics
 // this is the navbar logics
 // this is the navbar logics
+
+// this is the header section logics
+// this is the header section logics
+// this is the header section logics
+// this is the header section logics
+// this is the header section logics
